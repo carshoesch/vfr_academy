bce417006d9d6c8e10293fa0167c22130d42b265
carshoesch <hcarsten1@googlemail.com>
2023-02-22 15:25:08 +0100

    added font-family and active nav styling

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import Button from '../Button/Button'
 
+import Button from '../Button/Button'
 import styles from './Card.module.scss'
 
 const Card = ({ id, title, position, descriptionText, image, cardLink }) => {
--- src/components/Nav/DesktopLinks/DesktopLinks.jsx
+++ src/components/Nav/DesktopLinks/DesktopLinks.jsx
@@ -7,14 +7,46 @@ import styles from '../DesktopLinks/DesktopLinks.module.scss'
 const DesktopLinks = () => {
     const location = useLocation()
     const handleActiveLink = () => {
-        console.log('location', location);
+        // console.log('location', location);
+        // switch (location) {
+        //     case '/':
+        //         location === 
+        // }
     }
     return (
         <ul className={`${styles.navLinks}`}>
-            <li><NavLink onClick={handleActiveLink} className={styles.navLink} to="/">Home</NavLink></li>
-            <li><NavLink onClick={handleActiveLink} className={styles.navLink} to="/team">Team</NavLink></li>
-            <li><NavLink onClick={handleActiveLink} className={styles.navLink} to="/about">About the club</NavLink></li>
-            <li><NavLink onClick={handleActiveLink} className={styles.navLink} to="/contact">Contact</NavLink></li>
+            <li><NavLink onClick={handleActiveLink}
+                className={({ isActive }) => {
+                    const linkClasses = [styles.navLink];
+                    if (isActive) linkClasses.push(styles.active);
+
+                    return linkClasses.join(" "); // returns "registerButton" or "registerButton active"
+                }} to="/">Home</NavLink>
+            </li>
+            <li><NavLink onClick={handleActiveLink}
+                className={({ isActive }) => {
+                    const linkClasses = [styles.navLink];
+                    if (isActive) linkClasses.push(styles.active);
+
+                    return linkClasses.join(" "); // returns "registerButton" or "registerButton active"
+                }} to="/team">Team</NavLink>
+            </li>
+            <li><NavLink onClick={handleActiveLink}
+                className={({ isActive }) => {
+                    const linkClasses = [styles.navLink];
+                    if (isActive) linkClasses.push(styles.active);
+
+                    return linkClasses.join(" "); // returns "registerButton" or "registerButton active"
+                }} to="/about">About</NavLink>
+            </li>
+            <li><NavLink onClick={handleActiveLink}
+                className={({ isActive }) => {
+                    const linkClasses = [styles.navLink];
+                    if (isActive) linkClasses.push(styles.active);
+
+                    return linkClasses.join(" "); // returns "registerButton" or "registerButton active"
+                }} to="/contact">Contact</NavLink>
+            </li>
         </ul>
     )
 }
