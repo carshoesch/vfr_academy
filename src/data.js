3cba882ae5dbee31e86b5a20fb7a144ba3c22d79
carshoesch <hcarsten1@googlemail.com>
2023-02-26 13:01:10 +0100

    added new module data for detail page

--- src/data.js
+++ src/data.js
@@ -10,7 +10,22 @@ const cardData = [
         positionRaw: 'front_row',
         description: 'mit Michel Poppmaier, Headcoach Frankfur 1880 und ehemaliger deutscher Nationalspieler',
         image: dummy1,
-        // todo: need to redirect to a certain page based on the id of the object 
+        detailHeadline: 'Lerne worauf es in der ersten Reihe ankommt!',
+        detailDescription: 'in dem eintages Workshop mit Sturm-Experte Michael Popp-maier hast du die Chance zu lernen was du in deinem Training brauchst. Im fünf Modulen werden dir Inhalt zu Krafttraining, Therorie am Gedränge und in der Gasse soweie die Praktische umsetzung vermittelt.',
+        expertHeadline: 'Der Experte',
+        expertDescription: 'Michel Poppmaier ist Headcoach Frankfur 1880 und ehemaliger deutscher Nationalspieler in der ersten und zweiten Sturmreihe. Er ist als Spieler und Trainer mehrfach Deutscher-Meister geworden und bring seine internationale Expertiese mit.',
+        tableHeadline: 'Masterclass auf einem Blick',
+        modules: {
+            module1: 'Krafttraining für 1. Reihe-Spieler',
+            module2: 'Theorie Gedränge',
+            module3: 'Praxis Gedränge',
+            break: 'Mittagspause',
+            module4: 'Theorie Gasse',
+            module5: 'Praxis Gasse'
+        },
+        when: 'XX.XX.2023 von 10 - 18 Uhr',
+        where: 'VFR 06 Döhren, <br/>Gustav Brandt Str. 83 <br/>Hannover',
+        costs: '59,00 Euro <br/>inkl. Mittagsessen und Getränke'
     },
     {
         id: 1,
@@ -20,7 +35,17 @@ const cardData = [
         positionRaw: 'centre',
         description: 'mit Brian Habana, ehemaliger südafrikanischer Nationalspiler und Weltmeister von 2007',
         image: dummy2,
-        cardLink: 'https://www.google.de'
+        modules: {
+            module1: 'Krafttraining für 1. Reihe-Spieler',
+            module2: 'Theorie Gedränge',
+            module3: 'Praxis Gedränge',
+            break: 'Mittagspause',
+            module4: 'Theorie Gasse',
+            module5: 'Praxis Gasse'
+        },
+        when: 'XX.XX.2023 von 10 - 18 Uhr',
+        where: 'VFR 06 Döhren, <br/>Gustav Brandt Str. 83 <br/>Hannover',
+        costs: '59,00 Euro <br/>inkl. Mittagsessen und Getränke'
     },
     {
         id: 2,
@@ -30,7 +55,17 @@ const cardData = [
         positionRaw: 'second_row',
         description: 'mit Brian Habana, ehemaliger südafrikanischer Nationalspiler und Weltmeister von 2007',
         image: dummy2,
-        cardLink: 'https://www.google.de'
+        modules: {
+            module1: 'Krafttraining für 1. Reihe-Spieler',
+            module2: 'Theorie Gedränge',
+            module3: 'Praxis Gedränge',
+            break: 'Mittagspause',
+            module4: 'Theorie Gasse',
+            module5: 'Praxis Gasse'
+        },
+        when: 'XX.XX.2023 von 10 - 18 Uhr',
+        where: 'VFR 06 Döhren, <br/>Gustav Brandt Str. 83 <br/>Hannover',
+        costs: '59,00 Euro <br/>inkl. Mittagsessen und Getränke'
     },
     {
         id: 3,
@@ -40,7 +75,17 @@ const cardData = [
         positionRaw: 'flanker_nr8',
         description: 'mit Brian Habana, ehemaliger südafrikanischer Nationalspiler und Weltmeister von 2007',
         image: dummy2,
-        cardLink: 'https://www.google.de'
+        modules: {
+            module1: 'Krafttraining für 1. Reihe-Spieler',
+            module2: 'Theorie Gedränge',
+            module3: 'Praxis Gedränge',
+            break: 'Mittagspause',
+            module4: 'Theorie Gasse',
+            module5: 'Praxis Gasse'
+        },
+        when: 'XX.XX.2023 von 10 - 18 Uhr',
+        where: 'VFR 06 Döhren, <br/>Gustav Brandt Str. 83 <br/>Hannover',
+        costs: '59,00 Euro <br/>inkl. Mittagsessen und Getränke'
     },
     {
         id: 4,
@@ -50,7 +95,17 @@ const cardData = [
         positionRaw: 'scrumhalf',
         description: 'mit Brian Habana, ehemaliger südafrikanischer Nationalspiler und Weltmeister von 2007',
         image: dummy2,
-        cardLink: 'https://www.google.de'
+        modules: {
+            module1: 'Krafttraining für 1. Reihe-Spieler',
+            module2: 'Theorie Gedränge',
+            module3: 'Praxis Gedränge',
+            break: 'Mittagspause',
+            module4: 'Theorie Gasse',
+            module5: 'Praxis Gasse'
+        },
+        when: 'XX.XX.2023 von 10 - 18 Uhr',
+        where: 'VFR 06 Döhren, <br/>Gustav Brandt Str. 83 <br/>Hannover',
+        costs: '59,00 Euro <br/>inkl. Mittagsessen und Getränke'
     },
 ]
 
--- src/pages/detail/Detail.jsx
+++ src/pages/detail/Detail.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
+
 import styles from './Detail.module.scss'
 import data from '../../data'
+import Button from '../../components/Button/Button'
 
 const Detail = () => {
     // gets me my url parameter which is related to the specific detailpage
@@ -11,16 +13,53 @@ const Detail = () => {
     const paramDate = classId.split('+')[1]
     let detailPageData = {}
     // loop over every entry of data and check if position and date match
-    data.forEach(d => {
+    data.map(d => {
         if (d.positionRaw === paramPosition && d.creationDate === paramDate) {
             detailPageData = d
         }
+        return detailPageData
     })
+
+    const moduleData = Object.entries(detailPageData.modules).map(([key, value]) => (
+        // get modules object rendered out properly
+        JSON.stringify(`${key}: ${value}`)
+
+    ))
     console.log('detailPageData', detailPageData);
     return (
         <div className={styles.detailWrapper}>
-            {/* get screen design to visualize the object */}
-            Detail stuff: {JSON.stringify(detailPageData)}
+            {/* hero */}
+            <div className={styles.detailHeadlineDescription}>
+                <h2>{detailPageData.detailHeadline}</h2>
+                <p>{detailPageData.detailDescription}</p>
+                {/* where should this button lead? */}
+                <Button
+                    text={'Platz buchen'}
+                    btnWidth={'fit-content'}
+                />
+            </div>
+            {/* expert short bio */}
+            <div className={styles.detailExpertBio}>
+                <h2>{detailPageData.expertHeadline}</h2>
+                <div className={styles.expertCloseup}>
+                    <img src={detailPageData.image} alt="Expert" />
+                    <p>{detailPageData.expertDescription}</p>
+                </div>
+            </div>
+            {/* table of content */}
+            <div className={styles.detailMasterclass}>
+                <h2 className={styles.tableHeadline}>{detailPageData.tableHeadline}</h2>
+                <div className={styles.tableContent}>
+                    <div className={styles.tableModules}>
+                        {moduleData.map((module) => (
+                            module
+                        ))}
+                    </div>
+                    <div className={styles.tableInfos}>
+
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
